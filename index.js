cd39bbc5ba95004ee83c38758fb2e4c6615debb4
Bryan Arbelo <magestican.visualkei@gmail.com>
2018-02-22 15:08:47 +1100

    Update index.js

--- index.js
+++ index.js
@@ -3,32 +3,31 @@ import React from 'react';
 class ReactArrowHandler  extends React.Component {
 
   render() {
-    let {left,right,top,down} = this.props;
+    let {left,right,up,down} = this.props;
     let copies = React.Children.map(this.props.children[0], (child, i) => {
       let copy = React.cloneElement(child, {
-        onKeyDown: (e) => {
-            if (e.keyCode === 37){
-              if(left){
-                left();
-              }
-            } else if (e.keyCode === 39) {
-              if(right){
-                right();
-              }
-            } else if (e.keyCode === 38) {
-              if(up){
-                up();
-              }
-            } else if (e.keyCode === 40) {
-              if(down){
-                down();
-              }
-            }
-          }
       });
       return copy;
     });
-    return (<div>{copies}</div>);
+    return (<div onKeyDown={(e) => {
+        if (e.keyCode === 37){
+          if(left){
+            left();
+          }
+        } else if (e.keyCode === 39) {
+          if(right){
+            right();
+          }
+        } else if (e.keyCode === 38) {
+          if(up){
+            up();
+          }
+        } else if (e.keyCode === 40) {
+          if(down){
+            down();
+          }
+        }
+      }}>{copies}</div>);
   }
 }
 
